33eee55788f09fabac4f0bda415ff050bf6a83af
mxmlnkn <mxmlnkn@github.de>
2020-07-01 20:14:01 +0200

    Add ILSVRC 2010 dataset loader

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# tf-helpers
-Personal Tensorflow helpers for reading data and other things
+# Personal Tensorflow helpers for reading data and other things
+
+Contains:
+ - A tensorflow dataset build for ILSVRC2010 dataset.
+   The imagenet are all quite inflexible because they have things like validation lists, corrupted images and such hardcoded instead of checking them dynamically!
--- datasets/imagenet2010.py
+++ datasets/imagenet2010.py
@@ -16,20 +16,26 @@
 # Lint as: python3
 """Imagenet datasets."""
 
+# https://www.tensorflow.org/datasets/add_dataset
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import io
+import gzip
 import os
 import tarfile
+import tempfile
+
+import numpy as np
 
 import tensorflow.compat.v2 as tf
 import tensorflow_datasets.public_api as tfds
 
 
 _DESCRIPTION = '''\
-ILSVRC 2012, aka ImageNet is an image dataset organized according to the
+ILSVRC 2010 is an image dataset organized according to the
 WordNet hierarchy. Each meaningful concept in WordNet, possibly described by
 multiple words or word phrases, is called a "synonym set" or "synset". There are
 more than 100,000 synsets in WordNet, majority of them are nouns (80,000+). In
@@ -37,13 +43,12 @@ ImageNet, we aim to provide on average 1000 images to illustrate each synset.
 Images of each concept are quality-controlled and human-annotated. In its
 completion, we hope ImageNet will offer tens of millions of cleanly sorted
 images for most of the concepts in the WordNet hierarchy.
-
 Note that labels were never publicly released for the test set, so we only
 include splits for the training and validation sets here.
 '''
 
 # Web-site is asking to cite paper from 2015.
-# http://www.image-net.org/challenges/LSVRC/2012/index#cite
+# http://www.image-net.org/challenges/LSVRC/2010/index#cite
 _CITATION = '''\
 @article{ILSVRC15,
 Author = {Olga Russakovsky and Jia Deng and Hao Su and Jonathan Krause and Sanjeev Satheesh and Sean Ma and Zhiheng Huang and Andrej Karpathy and Aditya Khosla and Michael Bernstein and Alexander C. Berg and Li Fei-Fei},
@@ -57,64 +62,102 @@ pages={211-252}
 }
 '''
 
-_LABELS_FNAME = 'image_classification/imagenet2012_labels.txt'
+# The ID to label mapping can be extracted from the ILSVRC2010_devkit-1.0.tar.gz,
+# which contains data/meta.mat, using:
+#   python3 -c '
+#   import scipy.io
+#   meta = scipy.io.loadmat( "meta.mat" )
+#   with open( "ILSVRC2010_labels.txt", "wt" ) as file:
+#     for synset in meta["synsets"]:
+#       file.write( "{},{}\n".format( synset[0][0][0][0], synset[0][1][0] ) )
+#   '
+_LABELS_FNAME = 'ILSVRC2010_labels.txt'
 
 # This file contains the validation labels, in the alphabetic order of
 # corresponding image names (and not in the order they have been added to the
 # tar file).
-_VALIDATION_LABELS_FNAME = 'image_classification/imagenet2012_validation_labels.txt'
+_VALIDATION_LABELS_FNAME = 'ILSVRC2010_validation_ground_truth.txt'
+_TEST_LABELS_FNAME = 'ILSVRC2010_test_ground_truth.txt'
 
 
-# From https://github.com/cytsai/ilsvrc-cmyk-image-list
+# These lists can be easily calculated using find, xargs, ratarmount, and ImageMagick's identify or jpeginfo.
+# Note that identify takes multiple arguments and that should be made use of by using xargs' -n option
+# because starting identify up takes a lot of time by itself.
+# If your tar is on an SSD, prefer -P $( nprocs ) to speed up processing.
+# If your tar is on an HDD, prefer -P 1 to avoid too much seeking
+#   ratarmount ILSVRC2010_images_{train,val,test}.tar patch_images.tar /tmp/ILSVRC2010
+#   time nFiles=$( find /media/ILSVRC2010/ -type f | wc -l ) # 90s
+#   time find /media/ILSVRC2010/ -type f |
+#       pv --line-mode --size $nFiles |
+#       xargs -I{} -P 1 -n 100 jpeginfo -c {} 2>/dev/null |
+#       'grep' -v OK > jpeginfo.log # 30min
+
+# jpeginfo message: Corrupt JPEG data: 1 extraneous bytes before marker 0xd9  [WARNING]
+#   -> only a warning. shouldn't be so bad
+EXTRANEOUS_BYTES_IMAGES = [
+    'n01669191_10054.JPEG',
+    'n02882647_18811.JPEG',
+]
+# jpeginfo message: Not a JPEG file: starts with 0x1f 0x8b  [ERROR]
+#   -> This is a gz compressed jpeg ... -.-
+JPEG_GZ_IMAGES = [
+    'n02487347_1956.JPEG',
+]
+PNG_IMAGES = [] # None found. Seems to only be an issue in ILSVRC2012
+# jpeginfo message: Unsupported color conversion request  [ERROR]
 CMYK_IMAGES = [
+    'ILSVRC2010_test_00099846.JPEG',
+    'ILSVRC2010_test_00149181.JPEG',
+    'ILSVRC2010_val_00014712.JPEG',
+    'ILSVRC2010_val_00040086.JPEG',
+    'ILSVRC2010_val_00041418.JPEG',
     'n01739381_1309.JPEG',
-    'n02077923_14822.JPEG',
-    'n02447366_23489.JPEG',
-    'n02492035_15739.JPEG',
     'n02747177_10752.JPEG',
-    'n03018349_4028.JPEG',
-    'n03062245_4620.JPEG',
+    'n02872752_3721.JPEG',
+    'n03109150_15092.JPEG',
+    'n03329302_553.JPEG',
     'n03347037_9675.JPEG',
-    'n03467068_12171.JPEG',
-    'n03529860_11437.JPEG',
-    'n03544143_17228.JPEG',
+    'n03359436_9884.JPEG',
     'n03633091_5218.JPEG',
     'n03710637_5125.JPEG',
-    'n03961711_5286.JPEG',
     'n04033995_2932.JPEG',
-    'n04258138_17003.JPEG',
-    'n04264628_27969.JPEG',
-    'n04336792_7448.JPEG',
-    'n04371774_5854.JPEG',
+    'n04453156_5048.JPEG',
+    'n04517823_2387.JPEG',
     'n04596742_4225.JPEG',
-    'n07583066_647.JPEG',
-    'n13037406_4650.JPEG',
 ]
 
-PNG_IMAGES = ['n02105855_2933.JPEG']
 
+def script_folder():
+    return os.path.join( os.getcwd(), os.path.dirname( __file__ ) )
 
-class Imagenet2012(tfds.core.GeneratorBasedBuilder):
-  """Imagenet 2012, aka ILSVRC 2012."""
+class Imagenet2010(tfds.core.GeneratorBasedBuilder):
+  """ILSVRC 2010"""
 
-  VERSION = tfds.core.Version(
-      '5.0.0', 'New split API (https://tensorflow.org/datasets/splits)')
+  VERSION = tfds.core.Version('1.0.0')
 
   MANUAL_DOWNLOAD_INSTRUCTIONS = """\
-  manual_dir should contain two files: ILSVRC2012_img_train.tar and
-  ILSVRC2012_img_val.tar.
-  You need to register on http://www.image-net.org/download-images in order
-  to get the link to download the dataset.
+  manual_dir should contain these files:
+      ILSVRC2010_images_train.tar
+      ILSVRC2010_images_val.tar
+      ILSVRC2010_images_test.tar
+      patch_images.tar
+  In the past this dataset was downloadable from:
+      http://www.image-net.org/challenges/LSVRC/2010/download-all-nonpub
+      http://www.image-net.org/challenges/LSVRC/2010/ILSVRC2010_test_ground_truth.txt
+      The validation ground truth can be found in the ILSVRC2010_devkit-1.0.tar.gz.
+      The mapping between synsets and ground truth IDs can be found in ILSVRC2010_devkit-1.0/data/meta.mat
   """
 
   def _info(self):
-    names_file = tfds.core.get_tfds_path(_LABELS_FNAME)
+    labels = np.loadtxt( os.path.join( script_folder(), 'ILSVRC2010_labels.txt' ),
+                         delimiter = ',', dtype = str, encoding = 'utf-8' )
+    self._idToSynset = dict( zip( labels[:,0].astype( int ), labels[:,1] ) )
     return tfds.core.DatasetInfo(
         builder=self,
         description=_DESCRIPTION,
         features=tfds.features.FeaturesDict({
-            'image': tfds.features.Image(),
-            'label': tfds.features.ClassLabel(names_file=names_file),
+            'image': tfds.features.Image(encoding_format='jpeg'),
+            'label': tfds.features.ClassLabel(names=list(self._idToSynset.values())),
             'file_name': tfds.features.Text(),  # Eg: 'n15075141_54.JPEG'
         }),
         supervised_keys=('image', 'label'),
@@ -122,34 +165,49 @@ class Imagenet2012(tfds.core.GeneratorBasedBuilder):
         citation=_CITATION,
     )
 
-  @staticmethod
-  def _get_validation_labels(val_path):
-    """Returns labels for validation.
-
+  def _get_labels(self, archive_path, labels_path):
+    """Returns labels for validation and test.
     Args:
-      val_path: path to TAR file containing validation images. It is used to
+      archive_path: path to TAR file containing validation images. It is used to
       retrieve the name of pictures and associate them to labels.
-
     Returns:
       dict, mapping from image name (str) to label (str).
     """
-    labels_path = tfds.core.get_tfds_path(_VALIDATION_LABELS_FNAME)
     with tf.io.gfile.GFile(labels_path) as labels_f:
-      labels = labels_f.read().strip().split('\n')
-    with tf.io.gfile.GFile(val_path, 'rb') as tar_f_obj:
+      # `splitlines` to remove trailing `\r` in Windows
+      labels = [ self._idToSynset[int(x)] for x in labels_f.read().strip().splitlines() ]
+    with tf.io.gfile.GFile(archive_path, 'rb') as tar_f_obj:
       tar = tarfile.open(mode='r:', fileobj=tar_f_obj)
-      images = sorted(tar.getnames())
+      images = sorted([ tarinfo.name for tarinfo in tar if tarinfo.type == tarfile.REGTYPE])
+    assert len(images) == len(labels)
     return dict(zip(images, labels))
 
   def _split_generators(self, dl_manager):
-    train_path = os.path.join(dl_manager.manual_dir, 'ILSVRC2012_img_train.tar')
-    val_path = os.path.join(dl_manager.manual_dir, 'ILSVRC2012_img_val.tar')
-    # We don't import the original test split, as it doesn't include labels.
-    # These were never publicly released.
-    if not tf.io.gfile.exists(train_path) or not tf.io.gfile.exists(val_path):
+    train_path = os.path.join(dl_manager.manual_dir, 'ILSVRC2010_images_train.tar')
+    test_path = os.path.join(dl_manager.manual_dir, 'ILSVRC2010_images_test.tar')
+    val_path = os.path.join(dl_manager.manual_dir, 'ILSVRC2010_images_val.tar')
+    patch_path = os.path.join(dl_manager.manual_dir, 'patch_images.tar')
+
+    if tf.io.gfile.exists(patch_path):
+        # Extract patches so all into one folder so that it is easier to look them up by name
+        self.patch_images_folder_temp_object = tempfile.TemporaryDirectory()
+        self.patch_images_folder = self.patch_images_folder_temp_object.name
+        tar = tarfile.open(patch_path)
+        for tarInfo in tar:
+            image = tar.extractfile( tarInfo )
+            if image:
+                with open(os.path.join(self.patch_images_folder,
+                                       os.path.basename(tarInfo.name)), 'wb') as file:
+                    file.write( image.read() )
+    else:
+        self.patch_images_folder = None
+
+    if not tf.io.gfile.exists(train_path) or \
+       not tf.io.gfile.exists(test_path) or \
+       not tf.io.gfile.exists(val_path):
       raise AssertionError(
           'ImageNet requires manual download of the data. Please download '
-          'the train and val set and place them into: {}, {}'.format(
+          'the train, val, and test set and place them into: {}, {}'.format(
               train_path, val_path))
     return [
         tfds.core.SplitGenerator(
@@ -159,29 +217,39 @@ class Imagenet2012(tfds.core.GeneratorBasedBuilder):
             },
         ),
         tfds.core.SplitGenerator(
+            name=tfds.Split.TEST,
+            gen_kwargs={
+                'archive': dl_manager.iter_archive(test_path),
+                'labels': self._get_labels(test_path, os.path.join(script_folder(), _TEST_LABELS_FNAME)),
+            },
+        ),
+        tfds.core.SplitGenerator(
             name=tfds.Split.VALIDATION,
             gen_kwargs={
                 'archive': dl_manager.iter_archive(val_path),
-                'validation_labels': self._get_validation_labels(val_path),
+                'labels': self._get_labels(val_path, os.path.join(script_folder(), _VALIDATION_LABELS_FNAME)),
             },
         ),
     ]
 
   def _fix_image(self, image_fname, image):
-    """Fix image color system and format starting from v 3.0.0."""
-    if self.version < '3.0.0':
-      return image
-    if image_fname in CMYK_IMAGES:
+    patched_image_path = os.path.join(self.patch_images_folder, image_fname)
+    if self.patch_images_folder and tf.io.gfile.exists(patched_image_path):
+      # the patched images are all CMYK JPG, so no need to check against that!
+      image = io.BytesIO(open(patched_image_path, 'rb').read())
+    elif image_fname in CMYK_IMAGES:
       image = io.BytesIO(tfds.core.utils.jpeg_cmyk_to_rgb(image.read()))
+    elif image_fname in JPEG_GZ_IMAGES:
+      image = io.BytesIO(gzip.GzipFile(fileobj = image).read())
     elif image_fname in PNG_IMAGES:
       image = io.BytesIO(tfds.core.utils.png_to_jpeg(image.read()))
+
     return image
 
-  def _generate_examples(self, archive, validation_labels=None):
+  def _generate_examples(self, archive, labels=None):
     """Yields examples."""
-    if validation_labels:  # Validation split
-      for key, example in self._generate_examples_validation(archive,
-                                                             validation_labels):
+    if labels:  # Validation split
+      for key, example in self._generate_examples_validation(archive, labels):
         yield key, example
     # Training split. Main archive contains archives names after a synset noun.
     # Each sub-archive contains pictures associated to that synset.
@@ -205,7 +273,7 @@ class Imagenet2012(tfds.core.GeneratorBasedBuilder):
     for fname, fobj in archive:
       record = {
           'file_name': fname,
-          'image': fobj,
+          'image': self._fix_image(fname, fobj),
           'label': labels[fname],
       }
       yield fname, record
